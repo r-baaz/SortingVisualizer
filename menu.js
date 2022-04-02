6991c4ed157dba0bb6cbad85b85e645756838d77
niyazimohdarbaaz@gmail.com <niyazimohdarbaaz@gmail.com>
2022-04-02 19:31:34 +0530

    responsive

---
+++ menu.js
@@ -0,0 +1,5 @@
+let menubtn=document.getElementById("menubtn");
+    menubtn.addEventListener("click",()=>{
+        let sm=document.getElementById("sidemenu");
+        sm.style="left:0;";
+    });
